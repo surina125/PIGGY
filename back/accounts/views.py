bd9e1422251bcc190214534c0d6685da11e0bfc3
surina125 <jhyunsoo1125@gmail.com>
2024-05-24 02:15:15 +0900

    정규화뺌

--- back/accounts/views.py
+++ back/accounts/views.py
@@ -63,6 +63,11 @@ def recommend(request, username):
     # 입력 사용자의 벡터
     user_vector = user_data[['age', 'annual_income', 'property']].values[0]
 
+    ######### 여기서 정규화된 유클리드 거리를 사용하고 싶었지만 정규화과정 추가하면 시간이 너무 오래걸려서 뺐습니다.
+    # 거리 계산 및 정렬
+    other_users['distance'] = other_users.apply(lambda row: euclidean(user_vector, [row['age'], row['annual_income'], row['property']]), axis=1)
+    other_users = other_users.sort_values('distance')
+
     # 제품 목록을 중복 없이 합치기 위한 집합
     recommended_products = {
         'D_products': set(),
@@ -71,18 +76,14 @@ def recommend(request, username):
     }
 
     # 비슷한 사용자들로부터 제품 목록을 채우기
-    while len(recommended_products['D_products']) + len(recommended_products['S_products']) + len(recommended_products['L_products']) < 10 and not other_users.empty:
-        # 거리 계산 및 가장 가까운 사용자 찾기
-        other_users['distance'] = other_users.apply(lambda row: euclidean(user_vector, [row['age'], row['annual_income'], row['property']]), axis=1)
-        closest_user = other_users.loc[other_users['distance'].idxmin()]
+    for _, row in other_users.iterrows():
+        recommended_products['D_products'].update(set(row['D_products'].split(',') if row['D_products'] else []) - user_D_products)
+        recommended_products['S_products'].update(set(row['S_products'].split(',') if row['S_products'] else []) - user_S_products)
+        recommended_products['L_products'].update(set(row['L_products'].split(',') if row['L_products'] else []) - user_L_products)
 
-        # 추천 제품 목록 업데이트
-        recommended_products['D_products'].update(set(closest_user['D_products'].split(',') if closest_user['D_products'] else []) - user_D_products)
-        recommended_products['S_products'].update(set(closest_user['S_products'].split(',') if closest_user['S_products'] else []) - user_S_products)
-        recommended_products['L_products'].update(set(closest_user['L_products'].split(',') if closest_user['L_products'] else []) - user_L_products)
-
-        # 가장 가까운 사용자 제거
-        other_users = other_users.drop(closest_user.name)
+        total_products = len(recommended_products['D_products']) + len(recommended_products['S_products']) + len(recommended_products['L_products'])
+        if total_products >= 10:
+            break
 
     # 결과를 JSON 형태로 반환
     result = {
@@ -93,7 +94,7 @@ def recommend(request, username):
 
     return Response(result, status=status.HTTP_200_OK)
 
-    # 데이터베이스에서 모델의 모든 객체를 쿼리셋으로 불러오기
+    # # 데이터베이스에서 모델의 모든 객체를 쿼리셋으로 불러오기
     # queryset = User.objects.all().values(
     #     'id', 'username', 'age', 'annual_income', 'property', 'D_products', 'S_products', 'L_products'
     # )
@@ -121,10 +122,6 @@ def recommend(request, username):
     # # 입력 사용자의 벡터
     # user_vector = user_data[['age', 'annual_income', 'property']].values[0]
 
-    # # 거리 계산 및 정렬
-    # other_users['distance'] = other_users.apply(lambda row: euclidean(user_vector, [row['age'], row['annual_income'], row['property']]), axis=1)
-    # other_users = other_users.sort_values('distance')
-
     # # 제품 목록을 중복 없이 합치기 위한 집합
     # recommended_products = {
     #     'D_products': set(),
@@ -133,14 +130,18 @@ def recommend(request, username):
     # }
 
     # # 비슷한 사용자들로부터 제품 목록을 채우기
-    # for _, row in other_users.iterrows():
-    #     recommended_products['D_products'].update(set(row['D_products'].split(',') if row['D_products'] else []) - user_D_products)
-    #     recommended_products['S_products'].update(set(row['S_products'].split(',') if row['S_products'] else []) - user_S_products)
-    #     recommended_products['L_products'].update(set(row['L_products'].split(',') if row['L_products'] else []) - user_L_products)
+    # while len(recommended_products['D_products']) + len(recommended_products['S_products']) + len(recommended_products['L_products']) < 10 and not other_users.empty:
+    #     # 거리 계산 및 가장 가까운 사용자 찾기
+    #     other_users['distance'] = other_users.apply(lambda row: euclidean(user_vector, [row['age'], row['annual_income'], row['property']]), axis=1)
+    #     closest_user = other_users.loc[other_users['distance'].idxmin()]
 
-    #     total_products = len(recommended_products['D_products']) + len(recommended_products['S_products']) + len(recommended_products['L_products'])
-    #     if total_products >= 10:
-    #         break
+    #     # 추천 제품 목록 업데이트
+    #     recommended_products['D_products'].update(set(closest_user['D_products'].split(',') if closest_user['D_products'] else []) - user_D_products)
+    #     recommended_products['S_products'].update(set(closest_user['S_products'].split(',') if closest_user['S_products'] else []) - user_S_products)
+    #     recommended_products['L_products'].update(set(closest_user['L_products'].split(',') if closest_user['L_products'] else []) - user_L_products)
+
+    #     # 가장 가까운 사용자 제거
+    #     other_users = other_users.drop(closest_user.name)
 
     # # 결과를 JSON 형태로 반환
     # result = {
